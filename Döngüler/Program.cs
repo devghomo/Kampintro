de99782dd240f5743f4c7e45577b4c98d502b6f6
Pc <Pc@DESKTOP-01135QD>
2021-06-13 09:17:09 +0300

    Add project files.

---
+++ Döngüler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirci etiştirme Kampı";
+            string kurs2 = "Proglamlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+
+
+            string[] kurslar = new string[] { "Yazılım Geliştirci y etiştirme Kampı",
+            "Proglamlamaya başlangıç için temel kurs","Java","Python","C#" };
+
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)
+            {
+
+                Console.WriteLine(kurs);
+
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tape safety - tip güvenliği
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
